4fa0fb94b3a43a11330d22286a119c4a0d4fe005
JornabeDV <103864663+JornabeDV@users.noreply.github.com>
2022-08-03 17:27:34 -0300

    Correción de js

--- js/scripts.js
+++ js/scripts.js
@@ -128,42 +128,42 @@
 
 // alert("Hola "+ nombreCliente + "!"+" Gracias por elegir Préstamos BEJA.")
 
-function calcularPrestamo() {
-    document.getElementById("tab").innerHTML="";
-    let n= Number(document.getElementById("capital").value);
-    let n2= Number(document.getElementById("cuota").value);
-    let n3= Number(document.getElementById("interes").value);
-    if( n,n2,n3 >0 ) {   
-        for(i=1 ; i<=n2 ; i++) { // ([Expresión Inicial]; [Condicion]; [expresion Final]) {sentencia}
-            cuotas1=n/n2;  // Valor cuota por periodo. = Capital/Cuota.
-            d1=cuotas1.toFixed(2); // d1 = cuotas1 // obtenido de la tabla dinámica. 
-            i2=((n*n3)/100)/n2; // (([Capital dividido] * [Tasa de interes])/100)/ Cantidad de Cuotas) = valor interes
-            d2=i2.toFixed(2); // d2 = i2 // Valor interes por periodo de la tabla dinámica.
-            r=cuotas1+i2; // r es el valor a pagar por periodo.
-            d3=r.toFixed(2);//d3 = r obtenido de la tabla dinámica. d3 es periodico.
-            document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+ 
-                    `<tr>
-                        <td> ${i}</td>
-                        <td> ${d1}</td> 
-                        <td> ${d2}</td>
-                        <td> ${d3}</td>
-                    </tr>`;
-        }
-        n1=n.toFixed(2); // toFixed = es una function que deja los décimales que uno le establece en su parámetro. con punto para los decimales.
-        totalInteres=i2*n2;
-        d4=totalInteres.toFixed(2);
-        totalPeriodo=r*n2;
-        d5=totalPeriodo.toFixed(2);
-        document.getElementById("Total_Cuotas_Capital").innerHTML=n1;
-        document.getElementById("Total_Interes").innerHTML=d4;
-        document.getElementById("Total_a_Pagar").innerHTML=d5;        
-    }else{
-        alert("Falta ingresar algún importe");
-    }
-}
+// function calcularPrestamo() {
+//     document.getElementById("tab").innerHTML="";
+//     let n= Number(document.getElementById("capital").value);
+//     let n2= Number(document.getElementById("cuota").value);
+//     let n3= Number(document.getElementById("interes").value);
+//     if( n,n2,n3 >0 ) {   
+//         for(i=1 ; i<=n2 ; i++) { // ([Expresión Inicial]; [Condicion]; [expresion Final]) {sentencia}
+//             cuotas1=n/n2;  // Valor cuota por periodo. = Capital/Cuota.
+//             d1=cuotas1.toFixed(2); // d1 = cuotas1 // obtenido de la tabla dinámica. 
+//             i2=((n*n3)/100)/n2; // (([Capital dividido] * [Tasa de interes])/100)/ Cantidad de Cuotas) = valor interes
+//             d2=i2.toFixed(2); // d2 = i2 // Valor interes por periodo de la tabla dinámica.
+//             r=cuotas1+i2; // r es el valor a pagar por periodo.
+//             d3=r.toFixed(2);//d3 = r obtenido de la tabla dinámica. d3 es periodico.
+//             document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+ 
+//                     `<tr>
+//                         <td> ${i}</td>
+//                         <td> ${d1}</td> 
+//                         <td> ${d2}</td>
+//                         <td> ${d3}</td>
+//                     </tr>`;
+//         }
+//         n1=n.toFixed(2); // toFixed = es una function que deja los décimales que uno le establece en su parámetro. con punto para los decimales.
+//         totalInteres=i2*n2;
+//         d4=totalInteres.toFixed(2);
+//         totalPeriodo=r*n2;
+//         d5=totalPeriodo.toFixed(2);
+//         document.getElementById("Total_Cuotas_Capital").innerHTML=n1;
+//         document.getElementById("Total_Interes").innerHTML=d4;
+//         document.getElementById("Total_a_Pagar").innerHTML=d5;        
+//     }else{
+//         alert("Falta ingresar algún importe");
+//     }
+// }
 
-function solicitarContacto() {
+// function solicitarContacto() {
   
-    alert ("llamanos al 299-5249409");
+//     alert ("llamanos al 299-5249409");
 
-}
+// }
